0d404184b44d1dd210056878a0b19953eba5eae0
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-01-11 12:16:46 -0600

    added unincluded files

---
+++ client/components/frontend.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class Frontend extends React.Component {
+  render(){
+    return (
+      <div>Frontend</div>
+    )
+  }
+}
+
+  module.exports = Frontend;
---
+++ client/components/humanities.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class Humanities extends React.Component {
+  render(){
+    return (
+      <div>Humanities</div>
+    )
+  }
+}
+
+  module.exports = Humanities;
