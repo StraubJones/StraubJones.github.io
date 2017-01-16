f30cd33faabb9e6c7f96a10fd7bb43d4118c237a
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-01-16 14:33:08 -0600

    landing page mostly ready

--- client/components/home.js
+++ client/components/home.js
@@ -42,28 +42,40 @@ class Home extends React.Component {
           <i className="fa fa-2x fa-caret-down"></i>
         </button>
         <Element className="project-snapshot" name="first-project" id="first-project">
-          <img alt="project screenshot" src="./images/CollaBoard.png" />
           <h1>CollaBoard</h1>
-          <a href="https://collaboard-app.herokuapp.com/">SEE THE APP</a>
-          <a href="https://github.com/CollaBoard/CollaBoard">PERUSE THE CODE</a>
+          <h2>MY PART: SCRUM MASTER, REDUX, TEXT EDITOR</h2>
+          <img alt="project screenshot" src="./images/CollaBoardinAction.png" />
           <figcaption>Built with Postgres, React, Redux, Express, and Node </figcaption>
           <figcaption>Also uses Socket.io, HTML5 Canvas, WebRTC, Draft.js, and Prism.js </figcaption>
           <p>A live-synched whiteboard and text editor, with peer-to-peer video chat and typed chat. Ideal for remote classrooms, for remote team management, and for technical interviewing.</p>
-          <h4>My focus: Redux, Text editor with JS syntax highlighting (built on Draft.js, Prism.js), Scrum master leadership (led team meetings, curated project Github boards, and groomed backlog).</h4>
+          <a href="https://collaboard-app.herokuapp.com/">SEE THE APP</a>
+          <a href="https://github.com/CollaBoard/CollaBoard">PERUSE THE CODE</a>
         </Element>
         <button onClick={scrollToSecond} className="examples">
           EVEN MORE
           <i className="fa fa-2x fa-caret-down"></i>
         </button>
         <Element className="project-snapshot" name="second-project" id="second-project">
+          <h1>Tournament Manager</h1>
+          <h2>MY PART: PRODUCT OWNER, FIREBASE</h2>
           <img alt="project screenshot" src="./images/TournamentManager.png" />
+          <figcaption>Built with Firebase, React, Express, and Node </figcaption>
+          <p>A tournament manager for offline tournaments, for effortless brackets and stats management.</p>
+          <a href="https://github.com/MKS47GreenfieldAndLegacy/Game-Manager">PERUSE THE CODE</a>
         </Element>
         <button onClick={scrollToThird} className="examples">
           MORE STILL
           <i className="fa fa-2x fa-caret-down"></i>
         </button>
         <Element className="project-snapshot" name="third-project" id="third-project">
+          <h1>Intersectional Wage Gap Calculator and Graph Generator</h1>
+          <h2>MY PART: PRODUCT OWNER, D3, ANGULAR FRONT-END</h2>
           <img alt="project screenshot" src="./images/WageGapCalculator.png" />
+          <figcaption>Built with MongoDB, React, Express, and Node </figcaption>
+          <figcaption>Also uses D3, Angular Material</figcaption>
+          <p>Generates dynamic graphs regarding wage-gaps between various groups based on user-chosen variables.</p>
+          <a href="https://github.com/MKS47GreenfieldAndLegacy/Wage-Gap-Calculator">PERUSE THE CODE</a>
+
         </Element>
       </div>
     )
