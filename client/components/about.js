7700d0351c9fc63ccae92737809a6db2dcd5a2dc
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-01-27 17:35:37 -0600

    basic svg resizing for about chart

--- client/components/about.js
+++ client/components/about.js
@@ -1,21 +1,39 @@
 const React = require('react');
 const d3 = require('../vendor/d3.min.js');
 
+let chartWidth, chartHeight;
+
+let chartMargins = {
+  top: 20,
+  right: 50,
+  left: 50,
+  bottom: 50
+}
+
 class About extends React.Component {
+  //ideas: http://bl.ocks.org/mbostock/3885304
+  //more ideas (stretch goal): http://bl.ocks.org/mbostock/3885705
+  //good ideas about dimensions: http://blog.webkid.io/responsive-chart-usability-d3/#Use_aspect_ratio_instead_of_fixed_heightr
   componentDidMount(){
-    //ideas: http://bl.ocks.org/mbostock/3885304
-    //more ideas (stretch goal): http://bl.ocks.org/mbostock/3885705
+    this.updateChartSize(window.innerWidth);
+    window.addEventListener("resize", ()=>this.updateChartSize(window.innerWidth));
+  }
+  updateChartSize(winWidth) {
+    let self = this;
+    this.setState({
+      chartWidth: winWidth - chartMargins.right - chartMargins.left
+    }, ()=>{
+      self.setState({
+        chartHeight: self.state.chartWidth * 0.7
+      }, ()=>{
+        var vis = d3.select('.bar-graph')
+          .html("")
+          .append('svg')
+          .attr('width', this.state.chartWidth)
+          .attr('height', this.state.chartHeight);
+      });
 
-    var WIDTH = 800;
-
-    var HEIGHT = 500;
-
-    var vis = d3.select('.bar')
-      .append('svg')
-      .attr('width', WIDTH)
-      .attr('height', HEIGHT);
-
-
+    });
   }
   render(){
     return (
